d25a4622fa26fbc5b7ae63176652e9d03e3e455b
Iliya <ilij@ya.ru>
2019-05-09 03:49:41 +0300

    Лекция 4.2 3-20

--- app/Entity/Adverts/Category.php
+++ app/Entity/Adverts/Category.php
@@ -12,6 +12,18 @@ class Category extends Model
    public $timestamps=false;
    protected $fillable=['name','slug','parent_id'];
 
+    public function attributes()
+    {
+        return $this->hasMany(Attribute::class,'category_id','id');
+    }
+    public function parentAttributes()
+    {
+        return $this->parent ? $this->parent->allAttributes() : [];
+    }
+    public function allAttributes()
+    {
+        return array_merge($this->parentAttributes(), $this->attributes()->orderBy('sort')->getModels());
+    }
 
 
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,9 @@ const mix = require('laravel-mix');
 
 mix
     .setPublicPath('public/build')
-    .setResourceRoot('build')
+    .setResourceRoot('/build/')
     .js('resources/js/app.js', 'js')
    .sass('resources/sass/app.scss', 'css')
    .version();
+
+
